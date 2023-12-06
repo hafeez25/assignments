65074358d40802a31c0fb80758a8ef5b343a32ef
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-12-06 14:53:50 +0530

    anagram solve

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,30 @@
 */
 
 function isAnagram(str1, str2) {
+  const arr = new Array(256).fill(0);
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+  for (let i = 0; i < str1.length; i++) {
+    let num = str1.toLowerCase().charCodeAt(i);
+
+    arr[num]++;
+    console.log(arr[num]);
+  }
+  for (let i = 0; i < str2.length; i++) {
+    let num = str2.toLowerCase().charCodeAt(i);
+    arr[num]--;
+    console.log(arr[num]);
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    if (arr[i] != 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
