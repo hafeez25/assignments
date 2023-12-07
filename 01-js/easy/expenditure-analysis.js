d11c1da9e671fae99c92ac0f518663eca0959418
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-12-07 13:01:31 +0530

    total expenditure added

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,63 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const calculateTotal = {};
+
+  transactions.forEach((transaction) => {
+    const { category, price } = transaction;
+
+    if (calculateTotal[category]) {
+      calculateTotal[category].total += price;
+    } else {
+      calculateTotal[category] = {
+        total: price,
+      };
+    }
+  });
+  const result = Object.entries(calculateTotal).map(([category, total]) => {
+    return {
+      category,
+      totalSpent: total.total,
+    };
+  });
+
+  return result;
 }
+
+const transactions = [
+  {
+    itemName: "Item1",
+    category: "Groceries",
+    price: 50.0,
+    timestamp: "2023-01-01",
+  },
+  {
+    itemName: "Item2",
+    category: "Electronics",
+    price: 200.0,
+    timestamp: "2023-01-02",
+  },
+  {
+    itemName: "Item3",
+    category: "Groceries",
+    price: 30.0,
+    timestamp: "2023-01-03",
+  },
+  {
+    itemName: "Item4",
+    category: "Electronics",
+    price: 150.0,
+    timestamp: "2023-01-04",
+  },
+  {
+    itemName: "Item5",
+    category: "Clothing",
+    price: 100.0,
+    timestamp: "2023-01-05",
+  },
+];
+
+const result = calculateTotalSpentByCategory(transactions);
+console.log(result);
 
 module.exports = calculateTotalSpentByCategory;
