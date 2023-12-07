682bf20afe9e759d7d0fb9e8a03dfdbe590952bc
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-12-07 10:26:06 +0530

    solve palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,31 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase();
+
+  let start = 0;
+  let end = str.length - 1;
+  while (start <= end) {
+    let ch1 = str.charAt(start);
+    let ch2 = str.charAt(end);
+    while (ch1 < "a" || ch1 > "z") {
+      start++;
+      ch1 = str.charAt(start);
+    }
+    while (ch2 < "a" || ch2 > "z") {
+      end--;
+      ch2 = str.charAt(end);
+    }
+
+    if (ch1 !== ch2) {
+      return false;
+    }
+    start++;
+    end--;
+  }
   return true;
 }
+
+console.log(isPalindrome("Eva, can I see bees in a cave?"));
 
 module.exports = isPalindrome;
