261438dd1f9afee3d35b40d554fffee7ea05ba07
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-12-07 14:01:47 +0530

    expenditure solved

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,37 @@
 */
 
 class Todo {
-
+  tasks = [];
+  constructor() {}
+  add(task) {
+    this.tasks.push(task);
+  }
+  getAll() {
+    return this.tasks;
+  }
+  remove(index) {
+    this.tasks.splice(index, 1);
+  }
+  update(index, newTask) {
+    this.tasks[index] = newTask;
+  }
+  get(index) {
+    return this.tasks[index];
+  }
+  clear() {
+    this.tasks.splice(0);
+  }
 }
+
+const myTodo = new Todo();
+myTodo.add("HELOO Wordl");
+myTodo.add("HELOO Wordl");
+myTodo.add("HELOO Wordl");
+
+myTodo.remove(1);
+myTodo.remove(1);
+myTodo.update(0, "I'm Hafizur RAhman");
+myTodo.clear();
+console.log(myTodo.getAll());
 
 module.exports = Todo;
